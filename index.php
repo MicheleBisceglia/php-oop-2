fef83b476f269f5a7771f1b2deb6e6bb5c5da393
Michele Bisceglia <mikael_bisceglia@libero.it>
2022-06-14 16:11:37 +0200

    print products

--- index.php
+++ index.php
@@ -6,13 +6,65 @@ Il pagamento avviene con la carta di credito, che non deve essere scaduta.
 
 prodotto(nome,prezzo,codice) 
   {
-    cibo(animale, scadenza, tipologia)
-    giochi(materiali, tipologia)
-    trasporto(animale,materiele, misura)
+    cibo
+    giochi
+    trasporto
   }
 
 utente(nome, mail, scadenza carta) -->
 <?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+?>
+<?php
 require_once __DIR__ . "/Product.php";
 require_once __DIR__ . "/User.php";
 
+$dogFood = new Product("Cibo per cani", "$4.99", "Italy");
+$catFood = new Product("Cibo per gatti", "$3.99", "Italy");
+$catToy = new Product("Corda per gatti", "$2.99", "China");
+$dogBone = new Product("Osso finto per cani", "$7.99", "Italy");
+$transporCage = new Product("Cuccia da trasporto", "$39.99", "Germany");
+$transportBag = new Product("Borsa per trasporto", "$19.99", "Germany");
+
+
+$Utente1 = new User("Michele", "Mikael@lao.it", "20/09/2023");
+var_dump($Utente1)
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+  <!-- stampa provvisoria -->
+  <h1>Lista prodotti</h1>
+  <ul>
+    <li>
+      <?php echo $dogFood -> printProduct(); ?>
+    </li>
+    <li>
+      <?php echo $catFood -> printProduct(); ?>
+    </li>
+    <li>
+      <?php echo $catToy -> printProduct(); ?>
+    </li>
+    <li>
+      <?php echo $dogBone -> printProduct();?>
+    </li>
+    <li>
+      <?php echo $transporCage -> printProduct(); ?>
+    </li>
+    <li>
+      <?php echo $transportBag -> printProduct(); ?>
+    </li>
+  </ul>
+  
+</body>
+</html>
--- product.php
+++ product.php
@@ -2,12 +2,14 @@
 class Product {
   public $nome;
   public $prezzo;
-  public $id;
-  public $disponibile = true;
+  public $made;
 
-  function __construct($_nome, $_prezzo, $_id) {
-      $this->nome = $_nome;
-      $this->prezzo = $_prezzo;
-      $this->id = $_id;
+  function __construct($_nome, $_prezzo, $_made) {
+    $this->nome = $_nome;
+    $this->prezzo = $_prezzo;
+    $this->made = $_made;
+  }
+  public function printProduct() {
+    return "$this->nome $this->prezzo made in: $this->made";
   }
 }
